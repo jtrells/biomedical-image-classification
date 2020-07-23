9498205ee10fe8055c200d2d6c94fd77413ae14d
Juan Trelles <jtrell2@uic.edu>
2020-07-23 14:59:49 +0000

    reorganizing notebooks

--- src/utils/calc_mean.py
+++ src/utils/calc_mean.py
@@ -10,18 +10,18 @@ def calc_dataset_mean_std(train_tensor_dataset, batch_size=32, num_workers=0):
     loader = DataLoader(train_tensor_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
     
     mean = 0.0
-    for images, _ in loader:
+    for images, _, _ in loader:
         batch_samples = images.size(0)
         images = images.view(batch_samples, images.size(1), -1)
         mean += images.mean(2).sum(0)
     mean = mean / len(loader.dataset)
     
     var = 0.0
-    sample_img, _ = train_tensor_dataset[0]
+    sample_img, _, _ = train_tensor_dataset[0]
     # for std calculations
     h, w = sample_img.shape[1], sample_img.shape[2]
     
-    for images, _ in loader:
+    for images, _, _ in loader:
         batch_samples = images.size(0)
         images = images.view(batch_samples, images.size(1), -1)
         var += ((images - mean.unsqueeze(1))**2).sum([0,2])
