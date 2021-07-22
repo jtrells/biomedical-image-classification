a2e3ea67669c9897ff47ff80b56ecf5e2450f774
Juan Trelles <juantrelles@gmail.com>
2021-07-22 16:58:36 -0500

    datasets

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -2,6 +2,8 @@ from pandas import concat as pd_concat
 from pandas import read_csv
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 
+# There are no modalities for 
+
 def stratify(df, label_cols=['label', 'source'], n_splits=5):
     mskf = MultilabelStratifiedKFold(n_splits=n_splits, shuffle=False)
     for fold,(train_index, test_index) in enumerate(mskf.split(df, df[label_cols])):
@@ -21,26 +23,44 @@ def stratify(df, label_cols=['label', 'source'], n_splits=5):
     return df_concat
 
 
-def convert_clef_dataset(csv_path, clef_mapping):
+def convert_clef_openi_dataset(csv_path, dataset, mapping):
     df = read_csv(csv_path, sep='\t')
-    df = df[df.source.isin(['clef13', 'clef16'])]
+    if dataset == 'clef':
+        df = df[df.source.isin(['clef13', 'clef16'])]
+    else:
+        df = df[df.source.isin(['openi'])]
     # modality column has the original clef values
-    prefix, mapping = clef_mapping
-
-    df = df[df.modality[:len(prefix)] == prefix]
-    df['modality'] = df['modality'].replace(mapping)    
+    df = df[df.modality.isin(list(mapping.keys()))]
+    df['modality'] = df['modality'].replace(mapping)
 
     cols = ['img', 'source', 'img_path', 'caption', 'modality', 'split_set']
     df = df[cols]
-    df = df.rename(columns={'modality': 'label'})    
+    df = df.rename(columns={'modality': 'label'})
+    return df
+
+
+def get_radiology_dataset(csv_path):
+    mappings = radiology_mapping()
+    df_clef  = convert_clef_openi_dataset(csv_path, 'clef', mappings['clef'])
+    df_openi = convert_clef_openi_dataset(csv_path, 'openi', mappings['openi'])
+    df = pd_concat[df_clef, df_openi]
+    return stratify(df)
 
 
 def radiology_mapping():
-    return 'DR', {
-        'DRXR': 'X-RAY',
-        'DRUS': 'ULTRASOUND',
-        'DRAN': 'ANGIOGRAPHY',
-        'DRMR': 'CT/MRI/PET',
-        'DRPE': 'CT/MRI/PET',
-        'DRCO': 'OTHER'
+    return {
+        'clef': {
+            'DRXR': 'X-RAY',
+            'DRUS': 'ULTRASOUND',
+            'DRAN': 'ANGIOGRAPHY',
+            'DRMR': 'CT/MRI/PET',
+            'DRPE': 'CT/MRI/PET',
+            'DRCO': 'OTHER'
+        }, 
+        'openi': {
+            'DRXR': 'X-RAY',
+            'DRCT': 'CT/MRI/PET',
+            'DRUS': 'ULTRASOUND',
+            'DRMR': 'CT/MRI/PET'
+        }
     }
