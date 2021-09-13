9b1d3bcfb730675d0e8307ca82c9a6763e661725
Juan Trelles <juantrelles@gmail.com>
2021-09-13 12:43:16 -0500

    load_tree

--- src/flask.py
+++ src/flask.py
@@ -8,7 +8,8 @@ module_path = './'
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from os import getenv
+import pandas as pd
+from os import getenv, path, listdir
 from flask import Flask
 from flask_cors import CORS
 from markupsafe import escape
@@ -25,9 +26,13 @@ CORS(app)
 
 ROOT = getenv('FLASK_ROOT')
 
+vil_path = getenv('VIL_PATH')
+
+
 @app.route(ROOT + '/hello')
 def hello():
     return {"message": "hello"}
+
 
 @app.route(ROOT + "/images/<string:taxonomy>/<string:classifier>/<string:projection>/<string:version>/<string:subset>", methods=['GET'])
 def fetch_reduced_image_features(taxonomy, classifier, projection, version, subset):
@@ -38,7 +43,50 @@ def fetch_reduced_image_features(taxonomy, classifier, projection, version, subs
     subset = escape(subset)
 
     vil_path = getenv('VIL')
-    df = get_data(mongo.db, vil_path, taxonomy, classifier, projection, version=version, subset=subset, num_dimensions=2)
+    df = get_data(mongo.db, vil_path, taxonomy, classifier,
+                  projection, version=version, subset=subset, num_dimensions=2)
     df = df[["img", "img_path", "x", "y", "hits", "label", "prediction"]]
 
     return df.to_json(orient="records")
+
+
+def merge_dfs(df1, df2):
+    " Given two parquet dataframes, get the label for the deepes element in the taxonomy"
+    df1 = df1[["img", "label"]]
+    df2 = df2[["img", "label"]]
+
+    df3 = df1.merge(df2, on='img', how='outer')
+    df3.label_x = df3.label_x.fillna('')
+    df3.label_y = df3.label_y.fillna('')
+
+    # keep label with more details (larger ones)
+    df3['label'] = df3.apply(lambda x: x.label_y if len(
+        x.label_y) > len(x.label_x) else x.label_x, axis=1)
+    df3 = df3.drop(['label_x', 'label_y'], axis=1)
+
+    return df3
+
+
+@app.route(ROOT + '/tree/<string:taxonomy>', methods=['GET'])
+def load_tree(taxonomy):
+    taxonomy = escape(taxonomy)
+    taxonomy_info = mongo.db.taxonomies.find_one({"name": taxonomy})
+
+    if taxonomy_info:
+        taxonomy_tree = [x.label for x in taxonomy_info.modalities]
+        taxonomy_tree.append(taxonomy)
+
+        parquets_path = path.join(vil_path, taxonomy)
+        dfs = [pd.read_parquet(path.join(parquets_path, x))
+               for x in listdir(parquets_path) if x.endswith('.parquet')]
+        df = dfs[0]
+        for i in range(1, len(dfs)):
+            df = merge_dfs(df, dfs[i])
+        df = df.drop(['img'], axis=1)
+
+        return {
+            "taxonomy": taxonomy_tree,
+            "data": df.label.values
+        }
+    else:
+        return {}
