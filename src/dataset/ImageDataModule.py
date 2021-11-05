03a782e1d89462c54e1b9e3568780d9db30b60c3
Juan Trelles <juantrelles@gmail.com>
2021-11-05 15:37:52 -0500

    class weights calculated from label encoder instead of train_df unique values

--- src/dataset/ImageDataModule.py
+++ src/dataset/ImageDataModule.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from .ImageDataset import ImageDataset
 from sklearn.utils import class_weight
-from pytorch_lightning import Trainer,seed_everything
+from pytorch_lightning import Trainer, seed_everything
 from pathlib import Path
 
 
@@ -17,14 +17,14 @@ class ImageDataModule(pl.LightningDataModule):
                  batch_size,
                  data_path,
                  base_img_dir,
-                 seed = 42,
-                 image_transforms = [],
+                 seed=42,
+                 image_transforms=[],
                  num_workers=8,
                  target_class_col='SET',
                  caption_col='CAPTION',
                  modality_col='MODALITY',
                  path_col='PATH',
-                 shuffle_train = True):
+                 shuffle_train=True):
         super().__init__()
         self.batch_size = batch_size
         self.data_path = data_path
@@ -35,12 +35,11 @@ class ImageDataModule(pl.LightningDataModule):
         self.modality_col = modality_col
         self.path_col = path_col
         self.seed = seed
-        self.image_transforms_train  = image_transforms[0]
-        self.image_transforms_valid  = image_transforms[1]
-        self.image_transforms_test   = image_transforms[2]
+        self.image_transforms_train = image_transforms[0]
+        self.image_transforms_valid = image_transforms[1]
+        self.image_transforms_test = image_transforms[2]
         self.shuffle_train = shuffle_train
-        
-        
+
     def prepare_data(self):
         path = Path(self.data_path)
         if path.suffix.lower() == '.csv':
@@ -48,62 +47,66 @@ class ImageDataModule(pl.LightningDataModule):
         else:
             self.df = pd.read_parquet(self.data_path)
         # Always run the prepare data in order to get the same results in training
+
     def set_seed(self):
         seed_everything(self.seed)
+
     def setup(self):
-        train_df = self.df[self.df[self.target_class_col]=='TRAIN']                
+        train_df = self.df[self.df[self.target_class_col] == 'TRAIN']
         y_train = train_df[self.modality_col].values
         # calculate a class weight vector
-        self.class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
-        del train_df,y_train
-                            
-    def train_dataloader(self):   
-        
+        self.class_weights = class_weight.compute_class_weight(
+            'balanced', classes=self.le.classes_, y=y_train)
+        # self.class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+        del train_df, y_train
+
+    def train_dataloader(self):
+
         train_dataset = ImageDataset(
-                                csv_data_path    = self.data_path,
-                                label_encoder    = self.le,
-                                base_img_dir     = self.base_img_dir,
-                                data_set         = 'TRAIN',
-                                image_transform  = self.image_transforms_train,
-                                label_name       = self.modality_col,
-                                target_class_col = self.target_class_col,
-                                path_col         = self.path_col)
-        
-        return DataLoader(dataset     = train_dataset,
-                          batch_size  = self.batch_size,
-                          shuffle     = self.shuffle_train,
-                          num_workers = self.num_workers)
+            csv_data_path=self.data_path,
+            label_encoder=self.le,
+            base_img_dir=self.base_img_dir,
+            data_set='TRAIN',
+            image_transform=self.image_transforms_train,
+            label_name=self.modality_col,
+            target_class_col=self.target_class_col,
+            path_col=self.path_col)
+
+        return DataLoader(dataset=train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=self.shuffle_train,
+                          num_workers=self.num_workers)
 
     def val_dataloader(self):
-        
+
         val_dataset = ImageDataset(
-                                csv_data_path    = self.data_path,
-                                label_encoder    = self.le,
-                                base_img_dir     = self.base_img_dir,
-                                data_set         = 'VAL',
-                                image_transform  = self.image_transforms_valid,
-                                label_name       = self.modality_col,
-                                target_class_col = self.target_class_col,
-                                path_col         = self.path_col)
-                    
-        return DataLoader(dataset     = val_dataset,
-                          batch_size  = self.batch_size,
-                          shuffle     = False,
-                          num_workers = self.num_workers)
-    
-    def test_dataloader(self):        
-        
+            csv_data_path=self.data_path,
+            label_encoder=self.le,
+            base_img_dir=self.base_img_dir,
+            data_set='VAL',
+            image_transform=self.image_transforms_valid,
+            label_name=self.modality_col,
+            target_class_col=self.target_class_col,
+            path_col=self.path_col)
+
+        return DataLoader(dataset=val_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers)
+
+    def test_dataloader(self):
+
         test_dataset = ImageDataset(
-                                csv_data_path    = self.data_path,
-                                label_encoder    = self.le,
-                                base_img_dir     = self.base_img_dir,
-                                data_set         = 'TEST',
-                                image_transform  = self.image_transforms_valid,
-                                label_name       = self.modality_col,
-                                target_class_col = self.target_class_col,
-                                path_col         = self.path_col)
-        
-        return DataLoader(dataset     = test_dataset,
-                          batch_size  = self.batch_size,
-                          shuffle     = False,
-                          num_workers = self.num_workers)
+            csv_data_path=self.data_path,
+            label_encoder=self.le,
+            base_img_dir=self.base_img_dir,
+            data_set='TEST',
+            image_transform=self.image_transforms_valid,
+            label_name=self.modality_col,
+            target_class_col=self.target_class_col,
+            path_col=self.path_col)
+
+        return DataLoader(dataset=test_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers)
