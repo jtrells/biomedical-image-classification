ae95db654be9f7886f84d900371ed19ac7caa9bf
Juan Trelles <juantrelles@gmail.com>
2021-11-16 13:00:43 -0600

    return ms_metric

--- src/flask.py
+++ src/flask.py
@@ -47,7 +47,7 @@ def fetch_reduced_image_features(taxonomy, classifier, projection, version, subs
     df = get_data(mongo.db, vil_path, taxonomy, classifier,
                   projection, version=version, subset=subset, num_dimensions=2)
     df = df[["img", "img_path", "x", "y", "hits",
-             "label", "prediction", "width", "height", "full_label", "caption", "source", "orig_full_label", "pred_probs", "en_entropy"]]
+             "label", "prediction", "width", "height", "full_label", "caption", "source", "orig_full_label", "pred_probs", "ms_metric"]]
 
     return df.to_json(orient="records")
 
